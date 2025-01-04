17160f31bb00accceae5a9ef509bcc04b0ced01f
류수영 <ryuswimming@snu.ac.kr>
2025-01-05 00:10:56 +0900

    CustomColor가 HEX값을 받아서 Color를 반환하게 수정 (#9)

--- MemoWithTags/MemoWithTags/CustomColor.swift
+++ MemoWithTags/MemoWithTags/CustomColor.swift
@@ -26,36 +26,39 @@ extension Color {
     
     static let tagTextColor: Color = .init(red: 26 / 255, green: 26 / 255, blue: 27 / 255).opacity(0.8)
     
-    // HSB 색 표현법을 기준으로 hue를 받으면 saturation과 brightness를 계산해서 온전한 Tag Color를 반환한다.
-    static func tagBackgroundColor(hue: Int) -> Color {
+    init(hex: String) {
+        // HEX 문자열에서 # 제거
+        let cleanedHex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         
-        // 입력 hue 값의 유효성 검사
-        guard (0 <= hue) && (hue < 360) else {return .red}
+        // 기본 값 (0, 0, 0) (블랙)
+        var int: UInt64 = 0
+        Scanner(string: cleanedHex).scanHexInt64(&int)
         
-        // 채도(S)와 명도(B) 변수 초기화
-        let saturation: Double
-        let brightness: Double
+        let red, green, blue, alpha: Double
         
-        // H값에 따라 S와 B 설정
-        switch hue {
-        case 50..<200:
-            saturation = 0.30
-            brightness = 0.98
-        case 200..<230:
-            saturation = 0.28
-            brightness = 0.99
-        case 230..<360, 0..<20:
-            saturation = 0.26
-            brightness = 1.00
-        case 20..<50:
-            saturation = 0.28
-            brightness = 0.99
+        switch cleanedHex.count {
+        case 3: // 3글자짜리 HEX 표현 (#RGB)
+            red   = Double((int >> 8) & 0xF) / 15.0
+            green = Double((int >> 4) & 0xF) / 15.0
+            blue  = Double(int & 0xF) / 15.0
+            alpha = 1.0
+        case 6: // 6글자짜리 HEX 표현 (#RRGGBB)
+            red   = Double((int >> 16) & 0xFF) / 255.0
+            green = Double((int >> 8) & 0xFF) / 255.0
+            blue  = Double(int & 0xFF) / 255.0
+            alpha = 1.0
+        case 8: // 8글자짜리 HEX 표현 (#AARRGGBB)
+            alpha = Double((int >> 24) & 0xFF) / 255.0
+            red   = Double((int >> 16) & 0xFF) / 255.0
+            green = Double((int >> 8) & 0xFF) / 255.0
+            blue  = Double(int & 0xFF) / 255.0
         default:
-            saturation = 0.28
-            brightness = 0.99
+            red = 0
+            green = 0
+            blue = 0
+            alpha = 1
         }
         
-        return Color(hue: Double(hue) / 360.0, saturation: saturation, brightness: brightness)
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: alpha)
     }
-    
 }
