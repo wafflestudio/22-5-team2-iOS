08423a0c4f185bb184bb8396465fe71b451f4f56
Swimming-in-Seoul <ryuswimming@snu.ac.kr>
2025-01-06 17:19:48 +0900

    Editing 파일 구조 만듦

---
+++ MemoWithTags/MemoWithTags/View/SubView/EditingMemoView.swift
@@ -0,0 +1,18 @@
+//
+//  EditingMemoView.swift
+//  MemoWithTags
+//
+//  Created by Swimming Ryu on 1/6/25.
+//
+
+import SwiftUI
+
+struct EditingMemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditingMemoView()
+}
---
+++ MemoWithTags/MemoWithTags/View/SubView/EditingTagListView.swift
@@ -0,0 +1,18 @@
+//
+//  EditingTagListView.swift
+//  MemoWithTags
+//
+//  Created by Swimming Ryu on 1/6/25.
+//
+
+import SwiftUI
+
+struct EditingTagListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditingTagListView()
+}
